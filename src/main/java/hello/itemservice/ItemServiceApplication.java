750e942c3a3b89af39c5db5a622a4c78a73bbe5d
hesung <hsjo6864@gmail.com>
2022-08-05 20:51:29 +0900

    2022.08.05 Spring MVC Validation Study - Validation / Validator / WebDataBinder 학습 - 스프링 Validator 인터페이스를 상속받아 Validator를 빈으로 구현 - 컨트롤러에서 @InitBinder 및 @Validated 로 자동 Validation 가능 - 글로벌 Validator 설정도 가능하다.

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -4,7 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItemServiceApplication {
+public class ItemServiceApplication  {
+
+	/**
+	 * 글로벌 설정 테스트
+	 * implements WebMvcConfigurer
+ 	 */
+	/*
+	@Override
+	public Validator getValidator() {
+		return new ItemValidator();
+	}
+	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +26,12 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        log.info("init binder {}", dataBinder);
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -202,7 +210,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         //log.info("objectName = {}", bindingResult.getObjectName());
@@ -232,12 +240,35 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        //최초 오류(타입 오류 등)발생하면 오류 내용 바로 리턴 - 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("erros = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("erros = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "validation/v2/editForm";
     }
+
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
